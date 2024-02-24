714969fb607c384399b0b462438405b5a83ad058
NITIN PRABHAKARAN <82857282+nitinprabhakaran@users.noreply.github.com>
2024-02-24 07:26:42 +0530

    Update Query_datastores.py

--- Query_datastores.py
+++ Query_datastores.py
@@ -1,18 +1,11 @@
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
-from reportlab.lib.units import inch
-from tabulate import tabulate
 
 def create_pdf_with_tables(file_name, tables):
     # Create a PDF document
     pdf = SimpleDocTemplate(file_name, pagesize=letter)
     story = []
-
-    # Add black rectangle shape to canvas for background color
-    black_rect = Table([[('',)]], colWidths=[pdf.width], rowHeights=[pdf.height])
-    black_rect.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, -1), colors.black)]))
-    story.append(black_rect)
 
     # Generate tables and add them to the PDF
     for table_data in tables:
@@ -23,13 +16,6 @@ def create_pdf_with_tables(file_name, tables):
         style = TableStyle([
             ('TEXTCOLOR', (0, 0), (-1, -1), colors.white),  # Set text color to white
             ('BACKGROUND', (0, 0), (-1, -1), colors.black), # Set background color to black
-            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-            ('GRID', (0, 0), (-1, -1), 1, colors.black),
-            ('TEXTCOLOR', (2, 1), (2, -1), colors.green),  # Set font color for "Compliant" column to green
-            ('TEXTCOLOR', (2, 1), (2, -1), colors.red)     # Set font color for "Non-Compliant" column to red
         ])
 
         # Apply style to table
@@ -37,7 +23,6 @@ def create_pdf_with_tables(file_name, tables):
 
         # Add table to story
         story.append(table)
-        story.append(Spacer(1, 12))
 
     # Build PDF
     pdf.build(story)
