074a8f4d35e43ba5d9f1afa60b1ae49cde784346
NITIN PRABHAKARAN <82857282+nitinprabhakaran@users.noreply.github.com>
2024-06-13 15:28:02 +0530

    Update Query_datastores.py

--- Query_datastores.py
+++ Query_datastores.py
@@ -1,71 +1,31 @@
----
-description: "Search for an AMI by name, launch an instance with a specified ENI, and attach a security group"
-schemaVersion: '0.3'
-assumeRole: "{{ AutomationAssumeRole }}"
-parameters:
-  AmiName:
-    type: String
-    description: "Name of the AMI to search for"
-  SubnetId:
-    type: String
-    description: "The subnet ID where the instance will be launched"
-  EnclaveSecurityGroupId:
-    type: String
-    description: "The ID of the security group to attach to the instance"
-  InstanceType:
-    type: String
-    description: "The instance type to use for the new instance"
-    default: "t2.micro"
-  EnclaveElasticNetworkInterfaceId:
-    type: String
-    description: "The ENI ID to attach to the instance"
+#!/bin/bash
 
-mainSteps:
-  - name: searchAMI
-    action: aws:executeAwsApi
-    inputs:
-      Service: ec2
-      Api: DescribeImages
-      Filters:
-        - Name: "name"
-          Values: ["{{ AmiName }}"]
-    outputs:
-      - Name: ImageId
-        Selector: "$.Images[0].ImageId"
-        Type: String
+# Function to increment IP address
+increment_ip() {
+    local IFS=.
+    local ip=($1)
+    ip[3]=$((ip[3]+2))
 
-  - name: launchInstance
-    action: aws:runInstances
-    maxAttempts: 3
-    timeoutSeconds: 1200
-    inputs:
-      ImageId: "{{ searchAMI.ImageId }}"
-      InstanceType: "{{ InstanceType }}"
-      NetworkInterfaces:
-        - DeviceIndex: 0
-          NetworkInterfaceId: "{{ EnclaveElasticNetworkInterfaceId }}"
-      SubnetId: "{{ SubnetId }}"
-      MinCount: 1
-      MaxCount: 1
-      SecurityGroupIds:
-        - "{{ EnclaveSecurityGroupId }}"
+    for i in {3..0}; do
+        if [ ${ip[i]} -ge 256 ]; then
+            ip[i]=0
+            if [ $i -gt 0 ]; then
+                ip[$((i-1))]=$((ip[$((i-1))]+1))
+            fi
+        fi
+    done
 
-  - name: attachSecurityGroup
-    action: aws:executeAwsApi
-    inputs:
-      Service: ec2
-      Api: ModifyNetworkInterfaceAttribute
-      NetworkInterfaceId: "{{ EnclaveElasticNetworkInterfaceId }}"
-      Groups:
-        - "{{ EnclaveSecurityGroupId }}"
-      
-  - name: verifyInstance
-    action: aws:assertAwsResourceProperty
-    inputs:
-      Service: ec2
-      Api: DescribeInstances
-      InstanceIds:
-        - "{{ launchInstance.InstanceIds[0] }}"
-      PropertySelector: "$.Reservations[0].Instances[0].State.Name"
-      DesiredValues:
-        - "running"
+    echo "${ip[0]}.${ip[1]}.${ip[2]}.${ip[3]}"
+}
+
+# Check if an IP address is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <IP_ADDRESS>"
+    exit 1
+fi
+
+# Increment the provided IP address
+new_ip=$(increment_ip $1)
+
+# Output the new IP address
+echo "New IP Address: $new_ip"
